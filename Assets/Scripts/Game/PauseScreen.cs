4d1570b65495a3505c2cab56444173ff05f0c671
Ggtz25 <gabriel.gutierrez06@utrgv.edu>
2026-02-19 14:18:19 -0600

    main menu start

---
+++ Assets/Scripts/Game/PauseScreen.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}

c9f996196ae57616452c300e1386467fcd0f84bd
Ggtz25 <gabriel.gutierrez06@utrgv.edu>
2026-03-01 23:26:22 -0600

    Added High Scores and some ui stuff

--- Assets/Scripts/Game/GameOverController.cs
+++ Assets/Scripts/Game/GameOverController.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverController : MonoBehaviour
 {
     public GameObject gameOverPanel;
 
+    public TMP_Text scoreText;
+    public TMP_Text bestScoreText;
+
     public void ShowGameOver()
     {
         gameOverPanel.SetActive(true);
         Time.timeScale = 0f;
+
+        int finalScore = GameSession.Instance.Score;
+
+        HighScoreManager.AddScore(finalScore);
+
+        scoreText.text = "Score: " + finalScore;
+        bestScoreText.text = "High Score: " + HighScoreManager.GetBestScore();
     }
 
     public void PlayAgain()
